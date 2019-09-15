8a7e26c43d9c7b3dc71be79852c70842553c2008
zcmjs <zcmjs@wp.pl>
2019-09-15 12:30:53 +0200

    Rozszerzenie observable o complete oraz jej wywolanie

--- src/rxjs/BasicsWithErrorHandler.js
+++ src/rxjs/BasicsWithErrorHandler.js
@@ -17,5 +17,26 @@ const obs = Observable.create(observer => {
     observer.next(99)
 });
 console.log('Before Observable');
-obs.subscribe(data => {console.log(data)},
-              error => {console.log(error)} );
+obs.subscribe(data => {
+        console.log(data)
+    },
+    error => {
+        console.log(error)
+    });
+
+//ostatnie rozszerzenie observable o funkcje complete. JEli w trakcie wykonywania wystapi blad to saie nie wywola funkcja complete
+const obs3 = Observable.create(observer => {
+    observer.next(2137)
+    observer.next(111)
+    observer.complete()
+});
+console.log('Before Observable');
+obs3.subscribe(
+    data => {
+        console.log(data)
+    },
+    error => {
+        console.log(error)
+    },
+    () => console.log('Wykonanie funkcji complete')
+);
